b729c944fc072628329cd5efb0cd95dd48f86e25
Michał Matulka <therealmikz@gmail.com>
2014-09-26 14:30:33 +0200

    Submit formularza

--- src/PHPCon/WorkshopBundle/Controller/DefaultController.php
+++ src/PHPCon/WorkshopBundle/Controller/DefaultController.php
@@ -4,12 +4,28 @@ namespace PHPCon\WorkshopBundle\Controller;
 
 use PHPCon\WorkshopBundle\Form\WorkshopType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
-    public function indexAction()
+    /**
+     * 
+     * @param \PHPCon\WorkshopBundle\Controller\Request $request
+     * @return Response
+     */
+    public function indexAction(Request $request)
     {
         $form = $this->createForm(new WorkshopType());
+
+        $form->handleRequest($request);
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+
+            $em->persist($form->getData());
+            $em->flush();
+        }
+
         return $this->render(
             'PHPConWorkshopBundle:Default:index.html.twig',
             [
