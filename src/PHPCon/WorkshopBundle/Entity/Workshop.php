703835d53aebb80ad17ac0c540f0b681a99c8b39
Michał Matulka <therealmikz@gmail.com>
2014-09-26 14:09:42 +0200

    Wygenerowanie encji Workshop

---
+++ src/PHPCon/WorkshopBundle/Entity/Workshop.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace PHPCon\WorkshopBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Workshop
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="PHPCon\WorkshopBundle\Entity\WorkshopRepository")
+ */
+class Workshop
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="title", type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="date", type="datetime")
+     */
+    private $date;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     * @return Workshop
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string 
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set date
+     *
+     * @param \DateTime $date
+     * @return Workshop
+     */
+    public function setDate($date)
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    /**
+     * Get date
+     *
+     * @return \DateTime 
+     */
+    public function getDate()
+    {
+        return $this->date;
+    }
+}
