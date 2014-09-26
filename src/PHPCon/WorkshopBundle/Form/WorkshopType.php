b513b5821ade11898f2264db6854ff19cd86446f
Michał Matulka <therealmikz@gmail.com>
2014-09-26 14:12:53 +0200

    Wygenerowanie forma dla encji Workshop

---
+++ src/PHPCon/WorkshopBundle/Form/WorkshopType.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace PHPCon\WorkshopBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class WorkshopType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title')
+            ->add('date')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'PHPCon\WorkshopBundle\Entity\Workshop'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'phpcon_workshopbundle_workshop';
+    }
+}
